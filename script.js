482af983bce1166ff55753c23ecb33ee487a5ded
OrkoDzia11 <lukaorkoshneli0@gmail.com>
2024-10-13 18:59:45 +0400

    Homework1Finish

---
+++ script.js
@@ -0,0 +1,42 @@
+//Lesson1Java
+
+let number1 = 100;
+
+if (number1 < 50) {
+    console.log('<50') ;
+}
+
+else if (number1 >20) {
+    console.log('>20');
+}
+
+else {
+    console.log('error')
+}
+
+// 
+
+
+let name1 = "Mariam";
+
+if (name1 = 'Mariam') {
+
+    console.log(true) ;
+}
+
+else {
+    console.log(false);
+    
+}
+
+let Name2 = 'Mariam';
+
+switch (Name2) {
+    case "Mariam" :
+     console.log(true);
+        break;
+
+    default:
+     console.log(false)
+}
+   
